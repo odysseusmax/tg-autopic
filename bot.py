750f6691d0e7bf538b76f1112343328e2d097b19
odysseusmax <royschristy@gmail.com>
2019-10-12 08:05:19 +0000

    finished

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import os
 import time
+import datetime
 from pathlib import Path
 
 
 import httpx
+from pyrogram import Client, api
 
 
 def fetch_new_pic():
@@ -20,20 +22,45 @@ def fetch_new_pic():
     return profile_photo
 
 
+def fetch_fact():
+    
+    API_URL = "https://geek-jokes.sameerkumar.website/api"
+    
+    r = httpx.get(API_URL)
+    
+    data = r.text.replace('"', '')
+        
+    return data
+
+
 def initiate_pic_updation(app):
     
     pic = fetch_new_pic()
     
+    bio = fetch_fact()
+    
     app.set_profile_photo(pic)
+    
+    tim = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(minutes=30, hours=5)))
+    
+    update_data = {}
+                
+    update_data.update({'first_name':f"{tim.hour}:{tim.minute} | {tim.day}-{tim.month}-{tim.year}"})
+        
+    update_data.update({'last_name': "odysseusmax"})
+    
+    if len(bio) <= 70:
+        update_data.update({'about': bio})
+        
+    app.send(api.functions.account.UpdateProfile(**update_data))
     
     pic.unlink()
 
 
+
 def main():
     
-    from pyrogram import Client
-    
-    from config import Config
+    from myconfig import Config
     
     token = Config.USER_TOKEN
     
@@ -47,7 +74,7 @@ def main():
             
             print('one more done..')
             
-            time.sleep(70)
+            time.sleep(75)
         
     print('stopped...')
 
